63590356f266ab13103480ebb3cb1e894c2ef529
gj25 <goran.jovanovic@ventyx.abb.com>
2013-05-20 19:54:19 -0700

    minor test changes

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,9 +1,9 @@
 package test
 
 import org.specs2.mutable._
-
 import play.api.test._
 import play.api.test.Helpers._
+import org.specs2.execute.Pending
 
 /**
  * add your integration spec here.
@@ -14,15 +14,13 @@ class IntegrationSpec extends Specification {
   "Application" should {
     
     "work from within a browser" in {
+      
       running(TestServer(3333), HTMLUNIT) { browser =>
-
         browser.goTo("http://localhost:3333/")
 
         browser.pageSource must contain("Your new application is ready.")
-       
       }
     }
-    
   }
   
 }
